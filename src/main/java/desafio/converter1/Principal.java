4ab61bc24697097edf241014ae8892702193fcfd
Guilherme Mattos <gui_bone@hotmail.com>
2021-02-14 13:43:11 -0300

    Desafio

--- src/main/java/desafio/converter1/Principal.java
+++ src/main/java/desafio/converter1/Principal.java
@@ -2,6 +2,7 @@ package desafio.converter1;
 
 public class Principal {
     public static void main(String[]args){
+
         Arquivo.ler();
         Covid.processarConsolidado();
         Covid.imprimirSaidaJson();
